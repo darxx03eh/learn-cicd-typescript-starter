efc1bf2c7a6c00290f26326e628b456bd153b0bd
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-03-03 14:12:45 +0200

    another update

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,7 @@ import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 
 const __dirname = path.resolve();
 
-if (!config.api.port) {
-  console.error("PORT environment variable is not set");
-  process.exit(1);
-}
+const port = config.api.port || "8080";
 
 const app = express();
 app.use(express.json());
@@ -44,6 +41,6 @@ v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
 
-app.listen(config.api.port, () => {
-  console.log(`Server is running on port: ${config.api.port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
